751e9ce0cf94f599d3077fd4a41a788f4c2e3460
ikke09 <development@fam-bechtluft.de>
2022-01-08 14:36:07 +0100

    Command line options added

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ import {
 } from 'fs/promises';
 import fetch from 'node-fetch';
 import ics from 'ics';
+import {
+    Command
+} from 'commander/esm.mjs';
+import path from 'path';
 
-const apiUri = 'https://api.stuv.app/rapla/lectures/MOS-TINF20A';
+const apiUri = 'https://api.stuv.app/rapla/lectures/';
 
-const fetchAppointments = async () => {
-    console.log(`Fetching appointments from ${apiUri}`);
-    const res = await fetch(apiUri);
+const fetchAppointments = async (course) => {
+    const url = apiUri + course;
+    console.log(`Fetching appointments from ${url}`);
+    const res = await fetch(url);
     const data = await res.json();
     const appointments = data.filter(obj => obj.lecturer != "");
     return appointments;
@@ -16,7 +21,7 @@ const fetchAppointments = async () => {
 
 const stringToDateArray = (dateString) => {
     const date = new Date(dateString);
-    return [date.getFullYear(), date.getMonth()+1, date.getDate(), date.getHours(), date.getMinutes()];
+    return [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes()];
 }
 
 const toICS = (appointments) => {
@@ -37,16 +42,26 @@ const toICS = (appointments) => {
 
 const run = async () => {
     try {
-        const appointments = await fetchAppointments();
+        const program = new Command();
+        program
+            .option('-o, --output <type>', 'specify output path', './')
+            .option('-n, --name <type>', 'specify output file name', 'appointments.ics')
+            .option('-c, --course <type>', 'specify course', 'MOS-TINF20A')
+
+        program.parse(process.argv);
+        const options = program.opts();
+        const appointments = await fetchAppointments(options.course);
         const events = toICS(appointments);
         ics.createEvents(events, (err, value) => {
-            if(err){
+            if (err) {
                 console.error(err);
                 return;
             }
             console.log(`Success. Writing to file...`);
-            writeFile('appointments.ics', value);
-            console.log('Finished! Saved to appointments.ics');
+            const file = path.join(options.output, options.name);
+            console.log(`${file} used as output`);
+            writeFile(file, value);
+            console.log(`Finished!`);
         });
     } catch (err) {
         console.error(err);
